013b85d7b1a18fd92d6bf3e0f2ccd968265f0020
v.murzina <murzina.viktoria15@gmail.com>
2024-02-17 16:39:51 +0200

    Initial commit

---
+++ CarRentalService/CarRentalForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalService
+{
+    public partial class CarRentalForm : Form
+    {
+        public CarRentalForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
